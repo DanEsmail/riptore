bcb9d9668c1c454337be4b410cc7e75a82b8a7d2
Daniel Esmail <danesmail29@gmail.com>
2017-02-04 21:01:17 -0600

    changed attack system to work with grid

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 
 function spawnPostion(){
 	var postion = Math.ceil(Math.random()*8 +1)*50
-	return (postion + "px");
+	return postion;
 }
 
 var goblin = [];
+var item = [];
 
 function changeColor(newColor) {
   var elem = document.getElementById('test');
@@ -18,7 +19,13 @@ class Enemy {
 		this.offense = offense
 		this.name = name;
 	}
+	
+}
 
+class Item {
+	constructor(name){
+		this.name = name
+	}
 	
 }
 
@@ -38,7 +45,7 @@ function movement(arrow){
 				$("#charcter").css('left', "-=50");
 				riptore.x = parseInt($("#charcter").css('left'))/50;
 				console.log(riptore.x + "," + riptore.y)
-				console.log(goblin[0]);
+				attack(goblin)
 			}
 			
 			break;
@@ -47,6 +54,7 @@ function movement(arrow){
 				$("#charcter").css('top', "-=50");
 				riptore.y = parseInt($("#charcter").css('top'))/50;
 				console.log(riptore.x + "," + riptore.y)
+				attack(goblin)
 			}
 			break;
 		case 39:
@@ -54,6 +62,7 @@ function movement(arrow){
 				$("#charcter").css('left', "+=50");
 				riptore.x = parseInt($("#charcter").css('left'))/50;
 				console.log(riptore.x + "," + riptore.y)
+				attack(goblin)
 			}
 			break;
 		case 40:
@@ -61,39 +70,65 @@ function movement(arrow){
 				$("#charcter").css('top', "+=50");
 				riptore.y = parseInt($("#charcter").css('top'))/50;
 				console.log(riptore.x + "," + riptore.y)
+				attack(goblin)
 			}
 			break;
 	}
 }
 
-function attack(attacker, defender, name){
+/*function attack(attacker, defender, name){
 	if(defender.health - attacker.offense < 0){
 		$("#" + name).attr('src', './images/skull.png');
 	} else{
 	defender.health = Math.abs(attacker.offense - defender.health)
 	}
-}
+}*/
 
 
 function enemySpawn(num){
 	for(i = 0; i < num;i++){
-		goblin[i] = new Enemy(20, 5, "goblin" + i);
+		goblin[i] = new Enemy(10, 5, "goblin" + i);
+		goblin[i].x = spawnPostion()/50;
+		goblin[i].y = spawnPostion()/50;
+		
 		$("#map").append("<img id='" + goblin[i].name + "' src='./images/goblin.png'>")
-		$('#' + goblin[i].name).css({'position':'absolute','left':spawnPostion(),'top': spawnPostion()})
-		console.log(parseInt($('#' + goblin[i].name).css('top'))/50 + "," + parseInt($('#' + goblin[i].name).css('left'))/50)
+		$('#' + goblin[i].name).css({'position':'absolute','left':goblin[i].x*50 + 'px','top': goblin[i].y*50 + 'px'})
+		console.log(goblin[i].x + "," + goblin[i].y)
 	}
-	
-	
+}
+
+function attack(arr){
+	for(var i = 0; i < arr.length; i++){
+		if (riptore.x === arr[i].x && riptore.y === arr[i].y){
+			if(arr[i].health - riptore.offense <= 0){
+				$('#' + arr[i].name).remove();
+				console.log(arr[i])
+			}else{
+				arr[i].health -= riptore.offense;
+				movement(37)
+			}
+		}
+	}
 	
 }
 
 
+function itemSpawn(num){
+	for(i = 0; i < num;i++){
+		item[i] = new Item("item" + i);
+		$("#map").append("<img id='" + item[i].name + "' src='./images/wall.png'>")
+		console.log('#' + item[i].name);
+		$('#' + item[i].name).css({'position':'absolute','left':spawnPostion(),'top': spawnPostion()})
+		console.log(parseInt($('#' + item[i].name).css('top'))/50 + "," + parseInt($('#' + item[i].name).css('left'))/50)
+	}
+}
+
+
+
+
 $(document).ready(function(){
-	let goblin = new Enemy(20, 5);
 	
-	enemySpawn(6);
-	
-	
+	enemySpawn(7);
 	
 	
 	//setting charcter position
@@ -101,20 +136,6 @@ $(document).ready(function(){
 	riptore.y = parseInt($("#charcter").css('top'))
 	
 	
-	
-	//basic fighting mechanic
-	$('#enemyHealth').html(goblin.health);
-	$('#adventureHealth').html(riptore.health);
-	
-	$("#enemyAtk").on("click", function(){
-		console.log(Math.ceil(Math.random()*9 +1)*50)
-		//attack(goblin, riptore, 'adventure');
-		//$('#adventureHealth').html(riptore.health);
-	});
-	$("#adventureAtk").on("click", function(){
-		attack(riptore, goblin, 'enemy');
-		$('#enemyHealth').html(goblin.health);
-	});
 	
 	//basic movement
 	$(document).keydown(function(event){
